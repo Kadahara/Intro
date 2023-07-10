035b630b42fb4954e6db25164c8faaec48446480
User <User@WIN-E8F88FA2UKH>
2023-07-10 12:53:35 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+			#region CONSOLE
+			Console.BackgroundColor = ConsoleColor.Magenta; //цвет фона
+            Console.CursorLeft = 22;                        //Положение слево
+            Console.CursorTop = 5;                          //Положение сверху
+            Console.Write("Привет, Мир!\n");
+            Console.Write("Привет, Мир!\n");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;     //Цвет шрифта 
+            Console.Write("Привет, Мир!\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Beep(1000, 700);                        // Сигнал БИП! (Частота + продолжительность)
+			#endregion CONSOLE
+
+		}
+	}
+}
